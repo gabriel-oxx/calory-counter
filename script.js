dc28a9885429ce7931e9fb09b216ca8795f2b370
Gabriel <gabrielcsilvasg@gmail.com>
2024-01-13 13:49:01 -0300

    Adicionar função cleanInputString

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ const entryDropdown = document.getElementById("entry-dropdown");
 const addEntryButton = document.getElementById("add-entry");
 const clearButton = document.getElementById("clear");
 const output = document.getElementById("output");
+let isError = false;
+
+function cleanInputString(str) {
+  const regex = /[+-\s]/g;
+  return str.replace(regex, "");
+}
+
+console.log(cleanInputString("1+2-3"));
